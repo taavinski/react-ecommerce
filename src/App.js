e3f934545bf9b47d37be95db9740a4cb4f8fba60
taavinski <taavinski@icloud.com>
2020-05-24 13:12:56 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Switch, Route} from "react-router-dom"
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from "./components/Navbar"
+import ProductList from "./components/ProductList"
+import Details from "./components/Details"
+import Cart from "./components/cart/Cart"
+import Default from "./components/Default"
+import Modal from './components/Modal'
+
+
+function App() {
+  return (
+    <>
+      <Navbar /> 
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route exact path="/details" component={Details} />
+        <Route exact path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+      <Modal />
+    </>
+  );
+}
+
+export default App;
+    
